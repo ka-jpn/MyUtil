1cc81f39144e07e614633f763b25ca7f2040681e
角田智活 <tomo_hirakawa_aomori@outlook.jp>
2025-05-15 10:32:29 +0900

    一部書き直し

--- MyUtil.cs
+++ MyUtil.cs
@@ -1,31 +1,22 @@
 ﻿namespace MyUtil {
   public static class MyUtil {
-    public static async Task<string?> GetFileString(string filePath) {
-      if(File.Exists(filePath)) {
-        using StreamReader sr = new(filePath);
-        return await sr.ReadToEndAsync();
-      } else {
-        return null;
-      }
-    }
+    public static async Task<string?> GetFileString(string filePath) => File.Exists(filePath) ? await filePath.MyApplyF(async name => { using StreamReader sr = new(name); return await sr.ReadToEndAsync(); }) : null;
   }
   public static class 汎用拡張メソッド {
     ///<summary>文字列中の指定した文字の出現回数をカウントする</summary>
     public static int MyCountChar(this string s,char c) => s.Length - s.Replace(c.ToString(),"").Length;
     ///<summary>アクションを指定した回数実行する</summary>
-    public static void MyTimes(this int count,Action action) => Enumerable.Repeat(action,count).ToList().ForEach(action => action());
-    ///<summary>操作をしながら操作途中の値も含めてリストで返す</summary>
-    public static IEnumerable<T2> MyScan<T1, T2>(this IEnumerable<T1> input,Func<T2,T1,T2> next,T2 state) { yield return state; foreach(var item in input) { state = next(state,item); yield return state; } }
+    public static void MyTimes(this Action action,int count) => Enumerable.Repeat(action,count).ToList().ForEach(action => action());
     ///<summary>見つからなかった場合にFindIndexが-1を返すのをnullに置換</summary>
     public static int? MyGetIndex<T>(this IEnumerable<T> list,Predicate<T> match) => list.ToList().FindIndex(match).MyApplyF(v => v != -1 ? v : (int?)null);
     ///<summary>見つからなかった場合にFindIndexが-1を返すのをnullに置換</summary>
     public static int? MyGetLastIndex<T>(this IEnumerable<T> list,Predicate<T> match) => list.ToList().FindLastIndex(match).MyApplyF(v => v != -1 ? v : (int?)null);
     ///<summary>!演算子を使わない形の関数に置き換え用</summary>
     public static bool MyIsEmpty<T>(this IEnumerable<T> list) => !list.Any();
-    ///<summary>Actionを適用して返す メソッドチェーンをしたい時に有用 [例1]nullじゃない場合にメソッド呼び出し:str?.ApplyA(strList.Add);[例2]nullじゃない場合に操作:objList?.ApplyA(v=>v.clear());</summary>
-    public static T MyApplyA<T>(this T obj,Action<T> action) { action(obj); return obj; }
     ///<summary>Funcを適用 メソッドチェーンをしたい時に有用 [例]nullじゃない場合に操作:o=o?.ApplyF(v => v.a ="a"); .NETのバージョンアップで「o=o is not null ? o.a = "a" : null;」という書き方もできるようになった</summary>
     public static T2 MyApplyF<T1, T2>(this T1 obj,Func<T1,T2> func) => func(obj);
+    ///<summary>Actionを適用して返す メソッドチェーンをしたい時に有用 [例1]nullじゃない場合にメソッド呼び出し:str?.ApplyA(strList.Add);[例2]nullじゃない場合に操作:objList?.ApplyA(v=>v.clear());</summary>
+    public static T MyApplyA<T>(this T obj,Action<T> action) => obj.MyApplyF(v => { action(v); return v; });
     ///<summary>decimalのToStringのフォーマット指定の文字列を作成する</summary>
     public static string MyToNormalizeString(this decimal value,int decimalPlace) => value.ToString($"0.{Enumerable.Repeat("#",decimalPlace)}");
     ///<summary>Dictionaryをマージする</summary>
@@ -48,8 +39,6 @@
     public static List<T> MyNonNull<T>(this IEnumerable<T?> list) where T : class => [.. list.OfType<T>()];
     ///<summary>NullableなリストをNonNullなリストにする null要素は除外される</summary>
     public static List<T> MyNonNull<T>(this IEnumerable<T?> list) where T : struct => [.. list.OfType<T>()];
-    ///<summary>asyncなForEach</summary>
-    public static async Task ForEachAsync<T>(this List<T> list,Func<T,Task> func) { foreach(T value in list) { await func(value); } }
     ///<summary>Dictionaryにキーが存在しない場合のみ追加操作、操作後の値を返す</summary>
     public static Dictionary<T1,T2> MyAdd<T1, T2>(this Dictionary<T1,T2> map,T1 key,T2 newValue) where T1 : notnull => map.ToDictionary().MyApplyF(map => { map.TryAdd(key,newValue); return map; });
     ///<summary>Dictionaryにキーが存在する場合のみ更新操作、操作後の値を返す</summary>
@@ -60,6 +49,18 @@
     public static List<T> MyUpdate<T>(this List<T> list,int index,Func<T,T> newValueFactory) => (0 <= index && index < list.Count) ? list.ToList().MyApplyA(list => list[index] = newValueFactory(list[index])) : list;
     ///<summary>ListをListのListに分割して返す、分割する区切り箇所を決める条件関数を渡す</summary>
     public static List<List<T>> MyPartition<T>(this IEnumerable<T> list,Func<T,bool> cond) => list.Aggregate(new List<List<T>>(),(fold,elem) => cond(elem) ? [.. fold.Append([elem])] : [.. fold.SkipLast(1).Append([.. fold.Last().Append(elem)])]);
+    ///<summary>Nullableの値を要素数が1か0のリストに変換する</summary>
+    public static List<T> MyMaybeToList<T>(this T? maybeElem) where T : class => maybeElem != null ? [maybeElem] : [];
+    ///<summary>Nullableの値を要素数が1か0のリストに変換する</summary>
+    public static List<T> MyMaybeToList<T>(this T? maybeElem) where T : struct => maybeElem != null ? [maybeElem.Value] : [];
+    ///<summary>並列で実行されるAsync処理適用のForEach</summary>
+    public static async Task MyAsyncForEachParallel<T>(this IEnumerable<T> list,Func<T,Task> asyncAction) => await Task.WhenAll(list.Select(elem => Task.Run(async () => await asyncAction(elem))));
+    ///<summary>直列で実行されるAsync処理適用のForEach</summary>
+    public static void MyAsyncForEachSequential<T>(this IEnumerable<T> list,Func<T,Task> asyncAction) => list.ToList().ForEach(elem => asyncAction(elem).RunSynchronously());
+    ///<summary>操作途中の値も含めてリストで返す</summary>
+    public static IEnumerable<T2> MyScan<T1, T2>(this IEnumerable<T1> input,Func<T2,T1,T2> next,T2 state) => [.. input.Aggregate(new List<T2> { state },(stateList,item) => [.. stateList.Append(next(stateList.Last(),item))])];
+    ///<summary>操作をしながら操作途中の値も含めてリストで返す</summary>
+    public static IEnumerable<T2> MyScanLazy<T1, T2>(this IEnumerable<T1> input,Func<T2,T1,T2> next,T2 state) { yield return state; foreach(var item in input) { state = next(state,item); yield return state; } }
   }
   public class MyRandom {
     private static readonly Random rnd = new();
